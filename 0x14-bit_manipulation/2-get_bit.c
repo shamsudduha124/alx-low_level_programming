43fe05358673ec8c9edb3b5f3be486b06537e842
shamsudduha124 <shamsudduha124@gmail.com>
2023-09-28 15:31:08 -0700

    fixed betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * get_bits - returns the value of a bit at an index in a decimal number
+ * get_bit - returns the value of a bit at an index in a decimal number
  * @n: number to search
- * @index: index of the bits
+ * @index: index of the bit
+ *
  * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
