0718f933d8b09999a2df7d69bc6227978db2e157
shamsudduha124 <shamsudduha124@gmail.com>
2023-10-02 10:45:50 -0700

    2-append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	lekan = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	w = write(lekan, text_content, len);
 
 	if (lekan == -1 || w == -1)
 		return (-1);
